3c97313f21d0580ce62ed28e20af2e61e0cbbfbb
ValleST07 <135653738+ValleST07@users.noreply.github.com>
2025-04-02 11:25:53 +0200

    In WLAN einbinden

--- Programm/Programm.ino
+++ Programm/Programm.ino
@@ -2,13 +2,35 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <U8g2lib.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
-Adafruit_BME280 bme; // I2C
-
+Adafruit_BME280 bme;
 U8G2_SH1106_128X64_NONAME_F_HW_I2C display(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
+const char* ssid = "HTL-IoT";
+const char* password = "Internet0fThings!";
+WebServer server(80);
+
 unsigned long delayTime;
+
+void handleRoot() {
+    float temperature = bme.readTemperature();
+    float pressure = bme.readPressure() / 100.0F;
+    float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    float humidity = bme.readHumidity();
+    
+    String html = "<html><head><title>BME280 Sensor Data</title></head><body>";
+    html += "<h1>Sensor Daten</h1>";
+    html += "<p>Temperatur: " + String(temperature) + " &deg;C</p>";
+    html += "<p>Druck: " + String(pressure) + " hPa</p>";
+    html += "<p>H&ouml;he: " + String(altitude) + " m</p>";
+    html += "<p>Luftfeuchtigkeit: " + String(humidity) + " %</p>";
+    html += "</body></html>";
+    
+    server.send(200, "text/html", html);
+}
 
 void setup() {
     Serial.begin(9600);
@@ -17,29 +39,36 @@ void setup() {
 
     display.begin();
 
-    unsigned status;
-    status = bme.begin(0x76);
-    if (!status) {
+    if (!bme.begin(0x76)) {
         Serial.println("Could not find a valid BME280 sensor, check wiring!");
         while (1) delay(10);
     }
     
-    Serial.println("-- Default Test --");
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("Connected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.on("/", handleRoot);
+    server.begin();
+    Serial.println("HTTP server started");
+    
     delayTime = 1000;
 }
 
 void loop() {
+    server.handleClient();
+    
     float temperature = bme.readTemperature();
     float pressure = bme.readPressure() / 100.0F;
     float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
     float humidity = bme.readHumidity();
     
-    Serial.print("Temperature = "); Serial.print(temperature); Serial.println(" °C");
-    Serial.print("Pressure = "); Serial.print(pressure); Serial.println(" hPa");
-    Serial.print("Approx. Altitude = "); Serial.print(altitude); Serial.println(" m");
-    Serial.print("Humidity = "); Serial.print(humidity); Serial.println(" %");
-    Serial.println();
-
     display.clearBuffer();
     display.setFont(u8g2_font_ncenB08_tr);
     display.setCursor(0, 10);
@@ -52,6 +81,8 @@ void loop() {
     display.print("Humidity: "); display.print(humidity); display.print(" %");
     display.setCursor(0, 50);
     display.print("(c)by VS&ET");
+    display.setCursor(0, 60);
+    dispay.print(WiFi.localIP());
     display.sendBuffer();
     
     delay(delayTime);
